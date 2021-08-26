93310cb456299b948e0941cb7ea5cca053b2d55e
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-08-26 16:11:40 +0530

    pathMOD

---
+++ pathMOD.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+// console.log(
+//   path.dirname("C:/Users/alroyfernz/Desktop/ReactWeb/NODEjs/pathMOD.js")
+// );
+console.log(
+  path.parse("C:/Users/alroyfernz/Desktop/ReactWeb/NODEjs/pathMOD.js")
+);
